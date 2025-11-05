381a9554f024c178ffcb50851138a22507ca15ba
mhasaero <eroouji7314@gmail.com>
2025-11-05 21:48:07 +0700

    feat: add ProductListItem

--- ecommerce-bergerak/app/_layout.tsx
+++ ecommerce-bergerak/app/_layout.tsx
@@ -1,5 +1,11 @@
+import "@/global.css";
 import { Stack, Tabs } from "expo-router";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
-  return <Tabs />;
+  return (
+    <GluestackUIProvider>
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
--- ecommerce-bergerak/app/index.tsx
+++ ecommerce-bergerak/app/index.tsx
@@ -1,12 +1,21 @@
 import { View, Text, FlatList } from "react-native";
 import products from "../assets/products.json";
 import ProductItemList from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
+  // return (
+  //   <Button>
+  //     <ButtonText>Click Me</ButtonText>
+  //   </Button>
+  // );
   return (
     <View>
       <FlatList
         data={products}
+        numColumns={2}
+        contentContainerClassName="gap-2"
+        columnWrapperClassName="gap-2"
         renderItem={({ item }) => <ProductItemList product={item} />}
       />
     </View>
